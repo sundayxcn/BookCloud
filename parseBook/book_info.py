828008c80fa50e675beb17242331173754b8e794
sunday <szflone521@hotmail.com>
2017-06-10 22:20:18 +0800

    提交初始文件

---
+++ parseBook/book_info.py
@@ -0,0 +1,41 @@
+class BookDetail:
+    def __init__(self, dic_metas):
+        self.title = dic_metas['og:novel:book_name']
+        self.author = dic_metas['og:novel:author']
+        self.image = dic_metas['og:image']
+        self.category = dic_metas['og:novel:category']
+        self.read_url = dic_metas['og:novel:read_url']
+        self.latest_chapter_name = dic_metas['og:novel:latest_chapter_name']
+        self.latest_chapter_url = dic_metas['og:novel:latest_chapter_url']
+        self.description = dic_metas['og:description']
+        self.status = dic_metas['og:novel:status']
+        self.update_time = dic_metas['og:novel:update_time']
+
+    def printf(self):
+        print 'title                = ' + self.title
+        print 'author               = ' + self.author
+        print 'image                = ' + self.image
+        print 'category             = ' + self.category
+        print 'read_url             = ' + self.read_url
+        print 'latest_chapter_name  = ' + self.latest_chapter_name
+        print 'latest_chapter_url   = ' + self.latest_chapter_url
+        print 'description          = ' + self.description
+        print 'status               = ' + self.status
+        print 'update_time          = ' + self.update_time
+
+
+class BookChapter:
+    # list index
+    # chapterList
+    # def __init__(self, title, text, url):
+    #     self.title = title
+    #     self.text = text
+    #     self.url = url
+    def __init__(self):
+        self.title = ''
+        self.text = ''
+        self.url = ''
+
+    def printf(self):
+        print self.url + ' | ' + self.title
+
---
+++ parseBook/parse_book.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+#
+import scrapy
+import sys
+import re
+
+from book_info import BookDetail
+from book_info import BookChapter
+from downHtml import down_html
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+def parse_detail_from_meta(response):
+    metas_prop = response.xpath('//meta/@property').extract()
+    metas_value = response.xpath('//meta[@property]/@content').extract()
+    dic = {}
+    for i in range(len(metas_prop)):
+        dic[metas_prop[i]] = metas_value[i]
+    return dic
+
+
+def parse_chapter_list_from_dd(response):
+    chapter_list = []
+    chapter_url_list = response.xpath('//dd//a/@href').extract()
+    for url in chapter_url_list:
+        # 获取章节序号,存入数据库，真实网址拼接得来
+        # 用于排序和去重,
+        pattern = re.compile(r'\d[0-9]*\d\.')
+        str_url = pattern.findall(url)[0]
+        size = len(str_url) - 1
+        chapter = BookChapter()
+        chapter.url = str_url[0:size]
+        chapter_list.append(chapter)
+    chapter_title_list = response.xpath('//dd//a/text()').extract()
+    # for title in chapter_title_list:
+    #    print title
+    for i in range(0, len(chapter_title_list)):
+        chapter_list[i].title = chapter_title_list[i]
+
+    return chapter_list
+
+
+body = down_html.getHtml()
+scr_response = scrapy.Selector(text=body, type='html')
+
+dic = parse_detail_from_meta(scr_response)
+book_detail = BookDetail(dic)
+book_detail.printf()
+chapter_list = parse_chapter_list_from_dd(scr_response)
